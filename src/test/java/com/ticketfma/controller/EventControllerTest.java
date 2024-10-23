d3d0486b6a7068985688e71c9f0dc1386f750a90
Flavio Moises de Araujo <flavio.dearaujo@ticketmaster.com>
2024-10-23 19:27:26 -0400

    Initial structure, endpoints /events and /best-seats. Unit tests, swagger and csv load.

--- src/test/java/com/ticketfma/controller/EventControllerTest.java
+++ src/test/java/com/ticketfma/controller/EventControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -16,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.ticketfma.domain.Event;
 import com.ticketfma.domain.Seat;
 import com.ticketfma.service.EventService;
 
@@ -34,19 +36,37 @@ public class EventControllerTest {
     /* getEvents - BEGIN */
     @Test
     public void givenNoSort_whenGetEvents_thenReturnAllEvents() {
-        eventController.getEvents(Optional.empty());
+        List<Event> events = getEvents();
+        when(eventService.getAllEvents(Optional.empty())).thenReturn(events);
+
+        ResponseEntity<List<Event>> response = eventController.getEvents(Optional.empty());
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(events, response.getBody());
         verify(eventService).getAllEvents(Optional.empty());
     }
 
     @Test
     public void givenSortByName_whenGetEvents_thenReturnAllEventsSortedByName() {
-        eventController.getEvents(Optional.of("name"));
+        List<Event> events = getEvents();
+        when(eventService.getAllEvents(Optional.of("name"))).thenReturn(events);
+
+        ResponseEntity<List<Event>> response = eventController.getEvents(Optional.of("name"));
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(events, response.getBody());
         verify(eventService).getAllEvents(Optional.of("name"));
     }
 
     @Test
     public void givenSortByDate_whenGetEvents_thenReturnAllEventsSortedByDate() {
-        eventController.getEvents(Optional.of("date"));
+        List<Event> events = getEvents();
+        when(eventService.getAllEvents(Optional.of("date"))).thenReturn(events);
+
+        ResponseEntity<List<Event>> response = eventController.getEvents(Optional.of("date"));
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(events, response.getBody());
         verify(eventService).getAllEvents(Optional.of("date"));
     }
     /* getEvents - END */
@@ -54,7 +74,13 @@ public class EventControllerTest {
     /* getBestSeats - BEGIN */
     @Test
     public void givenValidEventIdAndQuantity_whenGetBestSeats_thenReturnBestSeats() {
-        eventController.getBestSeats(VALID_EVENT_ID, 5);
+        List<Seat> seats = getSeats();
+        when(eventService.getBestSeats(VALID_EVENT_ID, 5)).thenReturn(seats);
+
+        ResponseEntity<List<Seat>> response = eventController.getBestSeats(VALID_EVENT_ID, 5);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(seats, response.getBody());
         verify(eventService).getBestSeats(VALID_EVENT_ID, 5);
     }
 
@@ -65,6 +91,26 @@ public class EventControllerTest {
         ResponseEntity<List<Seat>> response = eventController.getBestSeats(INVALID_EVENT_ID, 5);
 
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        verify(eventService).getBestSeats(INVALID_EVENT_ID, 5);
     }
     /* getBestSeats - END */
+
+    /* stubs - BEGIN */
+    private List<Event> getEvents() {
+        return List.of(
+                Event.builder().eventId("1").eventDate(LocalDate.parse("2022-01-01")).name("Event 001").build(),
+                Event.builder().eventId("2").eventDate(LocalDate.parse("2022-01-02")).name("Event 002").build(),
+                Event.builder().eventId("3").eventDate(LocalDate.parse("2022-01-03")).name("Event 003").build()
+        );
+    }
+
+    private List<Seat> getSeats() {
+        return List.of(
+                Seat.builder().seatNumber("2").row("17").level("b").section("E").status("OPEN").sellRank(2).hasUpsells(false).build(),
+                Seat.builder().seatNumber("3").row("35").level("t").section("K").status("OPEN").sellRank(3).hasUpsells(false).build(),
+                Seat.builder().seatNumber("3").row("30").level("z").section("f").status("OPEN").sellRank(3).hasUpsells(false).build()
+
+        );
+    }
+    /* stubs - END */
 }
--- src/test/java/com/ticketfma/service/EventServiceTest.java
+++ src/test/java/com/ticketfma/service/EventServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -14,6 +16,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.ticketfma.domain.Event;
+import com.ticketfma.domain.Seat;
 import com.ticketfma.repository.IEventRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,9 +34,36 @@ public class EventServiceTest {
 
     /* getAllEvents - BEGIN */
     @Test
-    public void whenGetAllEvents_thenReturnAllEvents() {
-        eventService.getAllEvents(Optional.empty());
+    public void givenNoSort_whenGetAllEvents_thenReturnAllEvents() {
+        List<Event> events = getEvents();
+        when(repository.getAllEvents(Optional.empty())).thenReturn(events);
+
+        List<Event> allEvents = eventService.getAllEvents(Optional.empty());
+
+        assertEquals(events, allEvents);
         verify(repository).getAllEvents(Optional.empty());
+    }
+
+    @Test
+    public void givenSortByName_whenGetAllEvents_thenReturnAllEventsSortedByName() {
+        List<Event> events = getEvents();
+        when(repository.getAllEvents(Optional.of("name"))).thenReturn(events);
+
+        List<Event> eventsSortedByName = eventService.getAllEvents(Optional.of("name"));
+
+        assertEquals(events, eventsSortedByName);
+        verify(repository).getAllEvents(Optional.of("name"));
+    }
+
+    @Test
+    public void givenSortByDate_whenGetAllEvents_thenReturnAllEventsSortedByDate() {
+        List<Event> events = getEvents();
+        when(repository.getAllEvents(Optional.of("date"))).thenReturn(events);
+
+        List<Event> eventsSortedByDate = eventService.getAllEvents(Optional.of("date"));
+
+        assertEquals(events, eventsSortedByDate);
+        verify(repository).getAllEvents(Optional.of("date"));
     }
     /* getAllEvents - END */
 
@@ -50,10 +81,33 @@ public class EventServiceTest {
 
     @Test
     public void givenValidEventId_whenGetBestSeats_thenReturnBestSeats() {
+        List<Seat> seats = getSeats();
         when(repository.eventExists(VALID_EVENT_ID)).thenReturn(true);
+        when(repository.getBestSeats(VALID_EVENT_ID, 5)).thenReturn(seats);
 
-        eventService.getBestSeats(VALID_EVENT_ID, 5);
+        List<Seat> bestSeats = eventService.getBestSeats(VALID_EVENT_ID, 5);
+
+        assertEquals(seats, bestSeats);
         verify(repository).getBestSeats(VALID_EVENT_ID, 5);
     }
     /* getBestSeats - END */
+
+    /* stubs - BEGIN */
+    private List<Event> getEvents() {
+        return List.of(
+                Event.builder().eventId("1").eventDate(LocalDate.parse("2022-01-01")).name("Event 001").build(),
+                Event.builder().eventId("2").eventDate(LocalDate.parse("2022-01-02")).name("Event 002").build(),
+                Event.builder().eventId("3").eventDate(LocalDate.parse("2022-01-03")).name("Event 003").build()
+        );
+    }
+
+    private List<Seat> getSeats() {
+        return List.of(
+                Seat.builder().seatNumber("2").row("17").level("b").section("E").status("OPEN").sellRank(2).hasUpsells(false).build(),
+                Seat.builder().seatNumber("3").row("35").level("t").section("K").status("OPEN").sellRank(3).hasUpsells(false).build(),
+                Seat.builder().seatNumber("3").row("30").level("z").section("f").status("OPEN").sellRank(3).hasUpsells(false).build()
+
+        );
+    }
+    /* stubs - END */
 }
